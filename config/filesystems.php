284464569c38bc81011f15f7a96b3f097f396aea
市川マサル <>
2021-07-09 01:35:22 +0900

    files

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,15 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+        ],
+
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+            'visibility' => 'public',
+            'url' => env('APP_URL') . '/uploads',
         ],
 
         's3' => [
