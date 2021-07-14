08b1bd42d780b4680ba358eb8fe30dc79c6a9a20
市川マサル <>
2021-07-15 00:44:17 +0900

    test

--- resources/views/checktarekomis.blade.php
+++ resources/views/checktarekomis.blade.php
@@ -14,7 +14,7 @@
                             <tr>
                                 <td class="table-text">
                                     <div class="table-text">
-                                        <a href = "{{ route('article_tarekomi', ['tarekomi' => $item->title])}}">
+                                        <a href = "{{ route('admin.article_tarekomi', ['tarekomi' => $item->title])}}">
                                             {{$item -> title}}
                                         </a>
                                     </div>
