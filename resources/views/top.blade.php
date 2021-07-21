acbe9fe887e9a3150161f56fe144a0e3c584cc98
市川マサル <>
2021-07-21 22:20:01 +0900

    test

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('') }}</div>
+                <div class="card-header">{{ __('ダッシュボード') }}</div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
