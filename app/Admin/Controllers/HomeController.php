7353a01016cfba5552ec24e5a8b81ac3158b6407
市川マサル <>
2021-08-19 02:34:58 +0900

    admin削除機能

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -113,9 +113,7 @@ class HomeController extends Controller
     }
 
     public function alldelete(){
-        $deleteitem = Tarekomi::get();
-        // dd($deleteitem);
-        $deleteitem -> delete();
+        Tarekomi::query()->delete();
         return redirect("/admin/checktarekomis");
     }
 
