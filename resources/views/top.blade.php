be1c030c523a0d2aca3017509e811d28fe92eeb7
市川マサル <>
2021-08-30 02:15:14 +0900

    test

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <title>Document</title> -->
-</head>
-<body>
+
 @extends('layouts.app')
 @section('content')
 <div class="container">
@@ -35,7 +27,4 @@
     </div>
 </div>
 @endsection
-<script>
-</script>
-</body>
-</html>
+
